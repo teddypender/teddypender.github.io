39b29f6d403f7168b1fed20f452237e6a7a65f91
Teddy Pender <teddyjfpender@gmail.com>
2020-04-05 20:41:38 -0400

    Update index_diffusion.js

--- index_diffusion.js
+++ index_diffusion.js
@@ -4,7 +4,7 @@ let width = window.innerWidth;
 let height = window.innerHeight;
 
 const FLEE_RADIUS = 10;
-const BOIDS_COUNT = 700;
+const BOIDS_COUNT = 1000;
 const IMMUNE_COUNT = 200;
 const INFECTED_COUNT = 100;
 const HOSPITALS_COUNT = 0;
