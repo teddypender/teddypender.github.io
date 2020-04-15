c1071d62f09b206486ebac93a2e8e03a1ff164c0
Teddy Pender <teddyjfpender@gmail.com>
2020-04-15 14:37:30 -0400

    updated name

--- covid_dashboard/data_generation.py
+++ covid_dashboard/data_generation.py
@@ -375,7 +375,7 @@ bicycle_ur = "https://data.cityofnewyork.us/api/views/uczf-rk3c/rows.csv?accessT
 #nycBicycle_df = pd.read_csv(io.StringIO(nycBicycle.decode('utf-8')))
 
 
-
+csv_url = "https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1168&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=GDP&scale=left&cosd=1947-01-01&coed=2019-10-01&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Quarterly&fam=avg&fgst=lin&fgsnd=2009-06-01&line_index=1&transformation=lin&vintage_date=2020-04-15&revision_date=2020-04-15&nd=1947-01-01"
 
 #authorization
 gc = pygsheets.authorize(service_file='/Users/theodorepender/Desktop/covid19-dashboard-274000-97b3f9900832.json')
